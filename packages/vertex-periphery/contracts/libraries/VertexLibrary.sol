ba9eefd8c4d2871920909cc90deb14934c5ad581
Marc Velez <vert.exchange@protonmail.com>
2020-10-08 20:27:12 +0530

    init code hash changed in VertexLibrary for VertexPair.

--- packages/vertex-periphery/contracts/libraries/VertexLibrary.sol
+++ packages/vertex-periphery/contracts/libraries/VertexLibrary.sol
@@ -28,7 +28,7 @@ library VertexLibrary {
                         hex'ff',
                         factory,
                         keccak256(abi.encodePacked(token0, token1)),
-                        hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                        hex'e9af25cfd7086acf4bb3fc7e23e7e81b5e2db502c3924628a2d734e32a7edd2d' // init code hash
                     )
                 )
             )
