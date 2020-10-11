2433bc2878d445d6d83376b82b1e88f19be9879a
Marc Velez <vert.exchange@protonmail.com>
2020-10-11 10:27:02 +0530

    truffle setup and migration implemented in vertex-core.

---
+++ packages/vertex-core/migrations/1_deploy_vertex_factory.js
@@ -0,0 +1,10 @@
+var VertexFactory = artifacts.require('VertexFactory')
+
+module.exports = function(deployer, network, accounts) {
+  var _feeToSetter = '0x0000000000000000000000000000000000000000'
+
+  console.log(`Network: ${network}:`)
+  console.log(`Deployer: ${accounts[0]}`)
+
+  return deployer.deploy(VertexFactory, _feeToSetter, { from: accounts[0] })
+}
