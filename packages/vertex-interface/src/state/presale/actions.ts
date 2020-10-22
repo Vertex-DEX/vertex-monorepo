6021d2d3a912ca425dcb1a94cc92bc5af3f89bec
Marc Velez <vert.exchange@protonmail.com>
2020-10-22 16:46:20 +0530

    Presale pricing fixed.

--- packages/vertex-interface/src/state/presale/actions.ts
+++ packages/vertex-interface/src/state/presale/actions.ts
@@ -1,7 +1,7 @@
 import { createAction } from '@reduxjs/toolkit'
 
 export enum Field {
-  ETH = 'ETH',
+  ETHER = 'ETHER',
   VERTX = 'VERTX'
 }
 
--- packages/vertex-interface/src/state/presale/hooks.ts
+++ packages/vertex-interface/src/state/presale/hooks.ts
@@ -1,10 +1,64 @@
-import { useCallback } from 'react'
+import { useCallback, useMemo } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Currency, CurrencyAmount } from '@vertex/sdk'
 import { Field, typeInput } from './actions'
 import { AppDispatch, AppState } from '../index'
+import { tryParseAmount } from '../swap/hooks'
+import { useActiveWeb3React } from '../../hooks'
 
 export function usePresaleState(): AppState['presale'] {
   return useSelector<AppState, AppState['presale']>(state => state.presale)
+}
+
+export function useDerivedPresaleInfo(
+  etherCurrency: Currency | undefined | null,
+  vertxCurrency: Currency | undefined | null
+): {
+  independentField: Field
+  dependentField: Field
+  typedValue: string
+  formattedAmounts: { [field in Field]: string }
+} {
+  const { chainId } = useActiveWeb3React()
+
+  const { independentField, typedValue, otherTypedValue } = usePresaleState()
+
+  const dependentField = independentField === Field.ETHER ? Field.VERTX : Field.ETHER
+
+  // currencies
+  const currencies: { [field in Field]?: Currency } = useMemo(
+    () => ({
+      [Field.ETHER]: etherCurrency ?? undefined,
+      [Field.VERTX]: vertxCurrency ?? undefined
+    }),
+    [etherCurrency, vertxCurrency]
+  )
+
+  const independentAmount: CurrencyAmount | undefined = tryParseAmount(typedValue, currencies[independentField])
+  const dependentAmount: CurrencyAmount | undefined = useMemo(() => {
+    if (independentAmount) {
+      const fraction =
+        dependentField === Field.VERTX ? independentAmount.multiply('200000') : independentAmount.divide('200000')
+      return CurrencyAmount.ether(fraction.multiply('1000000000000000000').quotient)
+    } else {
+      return undefined
+    }
+  }, [otherTypedValue, currencies, dependentField, independentAmount, etherCurrency, chainId, vertxCurrency])
+
+  const independentFormattedAmount: string = independentAmount?.toSignificant(6) ?? ''
+  const dependentFormattedAmount: string = dependentAmount?.toSignificant(6) ?? ''
+
+  const formattedAmounts: { [field in Field]: string } = {
+    [Field.ETHER]: independentField === Field.ETHER ? independentFormattedAmount : dependentFormattedAmount,
+    [Field.VERTX]: independentField === Field.ETHER ? dependentFormattedAmount : independentFormattedAmount
+  }
+
+  return {
+    independentField,
+    dependentField,
+    typedValue,
+    formattedAmounts
+  }
 }
 
 export function usePresaleActionHandlers(): {
@@ -15,7 +69,7 @@ export function usePresaleActionHandlers(): {
 
   const onETHInput = useCallback(
     (typedValue: string) => {
-      dispatch(typeInput({ field: Field.ETH, typedValue }))
+      dispatch(typeInput({ field: Field.ETHER, typedValue }))
     },
     [dispatch]
   )
